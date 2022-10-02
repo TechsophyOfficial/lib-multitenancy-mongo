680a4ccad6419cd8172c67851ba11acb39d6c748
shashwattechsophy <94671615+shashwattechsophy@users.noreply.github.com>
2022-10-02 16:27:23 +0530

    Revert "Unit Test Cases"
    This reverts commit 86a8744dceac46af2f368b1fd7d04e6c7b10df07.

--- src/main/java/com/techsophy/multitenancy/mongo/config/MultiTenantMongoDBFactory.java
+++ src/main/java/com/techsophy/multitenancy/mongo/config/MultiTenantMongoDBFactory.java
@@ -15,14 +15,19 @@ public class MultiTenantMongoDBFactory extends SimpleMongoClientDatabaseFactory 
     @Value("${database.name}")
     private String databaseName;
 
+
+
+
     public MultiTenantMongoDBFactory(@Qualifier("createMongoClient") MongoClient mongoClient, String databaseName) {
         super(mongoClient, databaseName);
     }
 
     @Override
     protected MongoDatabase doGetMongoDatabase(String dbName) {
-        return getMongoClient().getDatabase(getTenantDatabase());
+        return  getMongoClient().getDatabase(getTenantDatabase());
     }
+
+
 
     @Override
     public MongoDatabase getMongoDatabase() {
