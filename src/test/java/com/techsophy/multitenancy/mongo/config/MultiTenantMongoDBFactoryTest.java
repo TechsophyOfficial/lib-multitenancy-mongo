ebecf761b6166e47b8ddbf95d874958f4c4a66ab
shashwattechsophy <94671615+shashwattechsophy@users.noreply.github.com>
2022-10-02 18:00:27 +0530

    Unit test (Changes based on suggestion)
    Test Case coverage upto 95%. added gradle config to check for test coverage up till 85%.

--- src/test/java/com/techsophy/multitenancy/mongo/config/MultiTenantMongoDBFactoryTest.java
+++ src/test/java/com/techsophy/multitenancy/mongo/config/MultiTenantMongoDBFactoryTest.java
@@ -1,25 +1,88 @@
 package com.techsophy.multitenancy.mongo.config;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.internal.MongoClientImpl;
+import com.mongodb.internal.async.client.AsyncClientSession;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MultiTenantMongoDBFactoryTest {
+class MultiTenantMongoDBFactoryTest{
+
+
+    @Mock
+    private MongoDatabase mockDB;
     @Mock
     MongoClient mongoClient;
+
     @InjectMocks
-    MultiTenantMongoDBFactory multiTenantMongoDBFactory;
+    SimpleMongoClientDatabaseFactory simpleMongoClientDatabaseFactory = Mockito.mock(SimpleMongoClientDatabaseFactory.class);
 
+    MultiTenantMongoDBFactory multiTenantMongoDBFactory = Mockito.mock(MultiTenantMongoDBFactory.class);
 
-//    @Test
-//    void doGetMongoDatabaseTest(){
-//       MongoDatabase response = multiTenantMongoDBFactory.doGetMongoDatabase("abc");
-//       Assertions.assertNull(response);
-//    }
+    @Test
+    void getMongoDatabaseTest(){
+        when(multiTenantMongoDBFactory.getMongoDatabase()).thenReturn(mockDB);
+        when(mongoClient.getDatabase(anyString())).thenReturn(mockDB);
+        Assertions.assertEquals(new MultiTenantMongoDBFactory(mongoClient,"abc").getMongoDatabase(),mockDB);
+        Assertions.assertEquals(multiTenantMongoDBFactory.getMongoDatabase(),mockDB);
+    }
+
+    @Test
+    void MultiTenantMongoDBFactoryTest(){
+        Assertions.assertNotNull(new MultiTenantMongoDBFactory(mongoClient,"abc"));
+    }
+
+    @Test
+    void SimpleMongoClientDatabaseFactoryTest(){
+
+        Assertions.assertEquals(simpleMongoClientDatabaseFactory.getMongoDatabase(),new SimpleMongoClientDatabaseFactory(mongoClient,"abc").getMongoDatabase());
+    }
+
+    @Test
+    void  getTenantDatabase(){
+        TenantContext.setTenantId("abc");
+        when(multiTenantMongoDBFactory.getTenantDatabase()).thenReturn(TenantContext.getTenantId());
+        Assertions.assertEquals("abc", multiTenantMongoDBFactory.getTenantDatabase());
+        Assertions.assertNotNull(multiTenantMongoDBFactory.getTenantDatabase());
+        Assertions.assertNull(new MultiTenantMongoDBFactory(mongoClient,"abc").doGetMongoDatabase("abc"));
+    }
+
+    @Test
+    void  doGetMongoDatabase(){
+        TenantContext.setTenantId("abc");
+        when(mongoClient.getDatabase(anyString())).thenReturn(mockDB);
+        Assertions.assertEquals(new MultiTenantMongoDBFactory(mongoClient,"abc").doGetMongoDatabase("abc"),mockDB);
+    }
+
+    @Test
+    void doGetMongoDatabaseNull(){
+        TenantContext.setTenantId(null);
+        when(mongoClient.getDatabase(null)).thenReturn(mockDB);
+        Assertions.assertEquals(new MultiTenantMongoDBFactory(mongoClient,"abc").doGetMongoDatabase("abc"),mockDB);
+    }
+
+    @Test
+    void tenantContextRemove(){
+        TenantContext.setTenantId("abc");
+        TenantContext.clear();
+        Assertions.assertEquals(null, TenantContext.getTenantId());
+    }
 }
